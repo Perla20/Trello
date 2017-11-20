ff5bf0401bb5d69efa98ecf6efeb82d7b36bd71b
Perla20 <perlablancoq@hotmail.com>
2017-11-20 03:29:12 -0500

    añadiendo estructura-estilo y funcionalidad

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function App() {
+  this.list = [];
+  this.newList = function() {
+
+    //Solicita información
+
+
+    var list = new List(name,isPrivate,starred);
+    this.lists.push(list);
+
+    return list;
+  }
